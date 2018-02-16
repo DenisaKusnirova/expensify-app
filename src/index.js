4e049f03b7f861daca4007d14ebe70431eec2251
Deniska Kusnirova <deniskakusnirova@gmail.com>
2018-02-16 09:44:52 +0100

    Add React-Router

--- src/index.js
+++ src/index.js
@@ -1,21 +1,8 @@
-// PROPS- allow the components to communicate
-// this- referece to the current instance of a component
-// STATE- allows our components to manage the data, it rerenders the app when
-// those data changes
-// STATELESS FUNCTIONAL COMPONENT:
-// const User = (props) => {
-//   return(
-//     <div>
-//       <p>Name: {props.name} </p>
-//       <p>Age: </p>
-//     </div>
-//   )
-// }
-
-import React from 'react';
-import ReactDOM from 'react-dom';
-import 'normalize.css/normalize.css';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import AppRouter from './routers/AppRouter'
+import 'normalize.css/normalize.css'
 import './styles/styles.scss'
 
 
-ReactDOM.render(<p>This is my boilerplate</p>, document.getElementById('app'))
+ReactDOM.render(<AppRouter />, document.getElementById('app'))
