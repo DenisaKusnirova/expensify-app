70a0b21e1c8fb743b8d293cca8b169a7afb5135f
Deniska Kusnirova <deniskakusnirova@gmail.com>
2018-03-21 15:40:57 +0100

    Set up public route

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,7 +6,7 @@ import { startLogOut } from '../actions/auth'
 const Header = ({ startLogOut }) => (
   <header>
     <h1>Expensify</h1>
-    <NavLink to="/" activeClassName="is-active" exact={true}>Dashboard</NavLink>
+    <NavLink to="/dashboard" activeClassName="is-active">Dashboard</NavLink>
     <NavLink to="/create" activeClassName="is-active">Create Expense</NavLink>
     <NavLink to="/help" activeClassName="is-active">Help Page</NavLink>
     <button onClick={startLogOut}>Logout</button>
