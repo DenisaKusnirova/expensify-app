f7f8370a9bebf4bed9f6360b334369cbbfa13f31
Deniska Kusnirova <deniskakusnirova@gmail.com>
2018-03-22 22:36:02 +0100

    Style expensify

--- src/components/EditExpensePage.js
+++ src/components/EditExpensePage.js
@@ -7,6 +7,12 @@ import { startEditExpense, startRemoveExpense } from '../actions/expenses'
 const EditExpensePage = (props) => {
   return (
     <div>
+      <div className="page-header">
+        <div className="content-container">
+          <h1 className="page-header__title">Edit Expense</h1>
+        </div>
+      </div>
+    <div className="content-container">
       <ExpenseForm
         expense={props.expense}
         onSubmit = {(expense) => {
@@ -15,11 +21,12 @@ const EditExpensePage = (props) => {
           console.log('updated', expense);
         }}
       />
-      <button onClick = {() => {
+      <button className="button button--secondary" onClick = {() => {
         props.dispatch(startRemoveExpense({ id: props.expense.id }))
         props.history.push('/')
         }}>Remove
       </button>
+    </div>
     </div>
   )
 }
